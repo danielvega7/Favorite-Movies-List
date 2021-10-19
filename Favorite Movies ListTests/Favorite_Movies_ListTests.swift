922ea046563e04ed2f46e99746b5662e9024c72a
Daniel Vega <drvega7@icloud.com>
2021-10-18 21:15:35 -0500

    Initial Commit

---
+++ Favorite Movies ListTests/Favorite_Movies_ListTests.swift
@@ -0,0 +1,33 @@
+//
+//  Favorite_Movies_ListTests.swift
+//  Favorite Movies ListTests
+//
+//  Created by Daniel Vega on 10/18/21.
+//
+
+import XCTest
+@testable import Favorite_Movies_List
+
+class Favorite_Movies_ListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
